584c84513acfe5ec91a7b6a4b065841bbd5f3828
Sam <samuel.moerman@gmail.com>
2024-11-27 20:43:37 -0500

    benchmark update

--- benchmark_bonds.sh
+++ benchmark_bonds.sh
@@ -31,11 +31,11 @@ for n in "${N_VALUES[@]}"; do
 
 
     # Run perf with repeats and capture elapsed times
-    # perf_output=$(/usr/lib/linux-tools/5.15.0-126-generic/perf stat -e cache-misses,cache-references --repeat $REPEATS $EXECUTABLE -n $n -s 1 -h $e 2>&1)
-    perf_output=$(perf stat -e cache-misses,cache-references --repeat $REPEATS $EXECUTABLE -n $n -s 1 -h $e 2>&1)
+    perf_output=$(/usr/lib/linux-tools/5.15.0-126-generic/perf stat -e cache-misses,cache-references --repeat $REPEATS $EXECUTABLE -n $n -s 1 -h $e 2>&1)
+    # perf_output=$(perf stat -e cache-misses,cache-references --repeat $REPEATS $EXECUTABLE -n $n -s 1 -h $e 2>&1)
 
     # Extract average elapsed time and append it to the grid
-    avg_time=$(echo "$perf_output" | awk '/seconds time elapsed/ {sum += $1; count++} END {if (count > 0) print sum / count}')
+    avg_time=$(echo "$perf_output" | awk '/seconds user/ {sum += $1; count++} END {if (count > 0) print sum / count}')
     echo -n ",$avg_time" >> $OUTPUT_FILE
   done
   echo >> $OUTPUT_FILE
